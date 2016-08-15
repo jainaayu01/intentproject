40d463f411b1358a9ef6e729e117fe5fea46e34e
aayushi jain <jjainprince01@gmail.com>
2016-08-15 13:47:13 +0530

    Navigation up button is visible and + button is also visible which is not visible earlier

--- app/src/main/java/com/example/india/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/india/criminalintent/CrimeLab.java
@@ -22,7 +22,14 @@ private List<Crime> mCrimes;
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+         for(int i=0;i<100;i++){
+            Crime crime = new Crime();
+             crime.setTitle("Crime #"+i);
 
+                 crime.setSolved(i%2==0);
+             mCrimes.add(crime);
+
+         }
 
     }
     public List<Crime> getCrimes(){
--- app/src/main/java/com/example/india/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/india/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
@@ -75,10 +76,20 @@ public class CrimeListFragment extends Fragment {
                 Intent intent = CrimePagerActivity.newIntent(getActivity(),crime.getUUID());
                 startActivity(intent);
                 return true;
+            case R.id.menu_item_show_subtitle:
+                   updateSubtitle();
+                return true;
                 default:
                     return super.onOptionsItemSelected(item);
         }
 
+    }
+    private void updateSubtitle(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle= getString(R.string.subtitle_format,crimeCount);
+        AppCompatActivity activity = (AppCompatActivity)getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
     /* private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private Crime mCrime;
